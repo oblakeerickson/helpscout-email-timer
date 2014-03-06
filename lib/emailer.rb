d0f2a193aa60417dbcd23c0b6b9f35768b84d327
Blake Erickson <o.blakeerickson@gmail.com>
2014-03-06 07:13:00 -0700

    add deliver method

--- lib/emailer.rb
+++ lib/emailer.rb
@@ -3,5 +3,16 @@ require 'mail'
 class Emailer
 
   def initialize
+    @mail = Mail.new do
+      from    'blake_test@example.com'
+      to      'o.blakeerickson@gmail.com'
+      subject 'This is a test email'
+      body    'How long did it take you to receive this message?'
+    end
+  end
+
+  def deliver
+    @mail.delivery_method :sendmail
+    @mail.deliver!
   end
 end
